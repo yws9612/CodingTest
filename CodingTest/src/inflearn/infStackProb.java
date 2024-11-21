7436de693a251a2a20e1cee784a68a97a0a7e771
yws9612 <3yws@naver.com>
2024-11-22 01:37:15 +0900

    인프런 스택 문제 풀이
    gpt 도움 받았으니까 해석하고 한번 자력으로 풀어보기

--- CodingTest/src/inflearn/infStackProb.java
+++ CodingTest/src/inflearn/infStackProb.java
@@ -1,11 +1,38 @@
 package inflearn;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class infStackProb {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         /*
         스택의 대표 유형 문제 괄호 유효성 검사
             ㄴ 이런 문제들을 템플릿 화 할 수 있을 정도로 많이 반복해서 풀어볼 것.
+        특정 조건이 만족돼 반응하는 문제의 경우 스택을 사용하면 좋음
          */
+        int [] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        int n = temperatures.length;
+        int[] ans = new int[n]; // 결과를 저장할 배열
+        Stack<int[]> stack = new Stack<>(); // 스택: {day, temp} 형태로 저장
 
+        for (int curDay = 0; curDay < n; curDay++) {
+            int curTemp = temperatures[curDay];
+
+            // 스택에 값이 있고 현재 온도가 스택의 마지막 온도보다 높은 경우
+            while (!stack.isEmpty() && stack.peek()[1] < curTemp) {
+                int[] prev = stack.pop();
+                int prevDay = prev[0];
+                ans[prevDay] = curDay - prevDay;
+            }
+
+            // 스택에 현재 날과 온도를 추가
+            stack.push(new int[]{curDay, curTemp});
+        }
+
+        for(int j = 0; j < ans.length; j++){
+            System.out.print(ans[j] + " ");
+        }
     }
 }
