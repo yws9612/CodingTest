035ce725f40b37828a53cce834d8d00dfefe4e6b
yws9612 <3yws@naver.com>
2024-11-27 15:20:22 +0900

    인프런 강의 정리
    DFS 알고리즘 코드 구현
    
    함수를 구현할 때 static을 사용 한 이유는 dfs 함수 호출 시 객체 생성 하지 않고 바로 사용하기 때문.

---
+++ CodingTest/src/inflearn/infPostorder.java
@@ -0,0 +1,48 @@
+package inflearn;
+
+public class infPostorder {
+
+    infNode root;
+
+    public static void main(String[] args) {
+        /*
+        Binary 트리에서 Lowest Common Ancestor(두 노드의 공통 조상 중 가장 낮은 node)를 찾는 문제
+
+        문제에서 Binary 트리 하나와 해당 트리에 속한 두개의 노드가 주어진다. 이 때, 두 노드의
+        공통 조상 중 가장 낮은 node 즉, the lowes common ancestor(LCA)를 찾아라.
+         */
+        infPostorder tree = new infPostorder();
+        tree.root = new infNode(3);
+        tree.root.lc = new infNode(5);
+        tree.root.rc = new infNode(1);
+        tree.root.lc.lc = new infNode(6);
+        tree.root.lc.rc = new infNode(2);
+        tree.root.lc.rc.lc = new infNode(7);
+        tree.root.lc.rc.rc = new infNode(4);
+        tree.root.rc.lc = new infNode(0);
+        tree.root.rc.rc = new infNode(8);
+
+        infNode value = dfs(tree.root, 6, 4);
+
+        System.out.println(value.value);
+    }
+
+    public static infNode dfs(infNode node, int p, int q){
+        if(node == null){
+            return null;
+        }else{
+             infNode left = dfs(node.lc, p, q);
+             infNode right = dfs(node.rc, p, q);
+
+             if(node.value == p || node.value == q){
+                 return node;
+             }else if(left != null && right != null){
+                 return node;
+             }else{
+                 return (left != null) ? left : right;
+             }
+
+        }
+    }
+
+}
